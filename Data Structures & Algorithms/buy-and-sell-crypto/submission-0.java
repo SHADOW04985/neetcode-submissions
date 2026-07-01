716a37159731ce5de3168ea25a5605cd423ca558
Aaryaman <aaryamanmaity@gmail.com>
2026-07-02 04:51:47 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for(int i : prices) {
+            if(i - min > profit) profit = i - min;
+            if(i < min) {
+                min = i;
+            }
+        }
+        return profit;
+
+    }
+}
